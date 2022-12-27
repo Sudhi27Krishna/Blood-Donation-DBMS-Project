6a88bf64aadc1d5dfa69c42e0e83a6ca4e56cfaf
Sudhi Krishna N A <sudhi27krishna@gmail.com>
2022-12-27 16:38:40 +0530

    Inserting,retrieving data from tables and displaying

--- app.js
+++ app.js
@@ -1,12 +1,30 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mysql = require("mysql2");
 
 const app = express();
 
 app.set('view engine', 'ejs');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static("public"));
+
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    port: 3306,
+    password: "123456",
+    database: "blood-db"
+});
+
+con.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    else {
+        console.log("Connected to database");
+    }
+});
 
 app.get("/", (req, res) => {
     res.render("home");
@@ -17,7 +35,11 @@ app.get("/register", (req, res) => {
 });
 
 app.get("/donors", (req, res) => {
-    res.render("donors");
+    let sql = 'SELECT * FROM person where role="donate";';
+    con.query(sql, function (error, results) {
+        if (error) throw error;
+        res.render("donors", { results: results });
+    });
 });
 
 app.get("/recipients", (req, res) => {
@@ -36,13 +58,36 @@ app.get("/register/register_h", (req, res) => {
     res.render("register_h");
 });
 
+app.post("/register/register_p", (req, res) => {
+    let cnt = 1;
+    const name = req.body.name;
+    const age = req.body.age;
+    const dob = req.body.dob;
+    const gender = req.body.gender;
+    const b_grp = req.body.b_grp;
+    const city = req.body.city;
+    const phone = req.body.phone;
+    const role = req.body.role;
 
+    let sql = "insert into person (name,age,dob,gender,bldgrp,city,phone,role) values('" + name + "'," + age + ",'" + dob + "','" + gender + "','" + b_grp + "','" + city + "','" + phone + "','" + role + "');";
+    con.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+        }
+        else {
+            console.log("Data inserted..");
+            console.log(result);
+            cnt++;
+            res.render("register_p");
+        }
+    });
+});
 
 app.listen(3000, (err) => {
-    if(err){
+    if (err) {
         console.log(err);
     }
-    else{
+    else {
         console.log("Server started on port 3000");
     }
 });
