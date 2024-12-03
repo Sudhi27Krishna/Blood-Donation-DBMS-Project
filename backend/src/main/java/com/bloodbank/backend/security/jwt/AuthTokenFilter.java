bb8831041817234bb5f9969b6702d0c175c9c937
Sudhi Krishna N A <sudhi27krishna@gmail.com>
2024-12-03 12:53:35 +0530

    Add jwt authentication

---
+++ backend/src/main/java/com/bloodbank/backend/security/jwt/AuthTokenFilter.java
@@ -0,0 +1,61 @@
+package com.bloodbank.backend.security.jwt;
+
+import com.bloodbank.backend.security.user.BBUserDetailsService;
+import io.jsonwebtoken.JwtException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.lang.NonNull;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class AuthTokenFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private BBUserDetailsService userDetailsService;
+
+    @Override
+    protected void doFilterInternal(@NonNull HttpServletRequest request,
+                                    @NonNull HttpServletResponse response,
+                                    @NonNull FilterChain filterChain) throws ServletException, IOException
+    {
+        try {
+            String jwt = parseJwtToken(request);
+            if(StringUtils.hasText(jwt) && jwtUtils.validateJwtToken(jwt)) {
+                String username = jwtUtils.getUsernameFromJwtToken(jwt);
+                UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+                Authentication auth = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            }
+        } catch (JwtException e) {
+            response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+            response.getWriter().write(e.getMessage() +" : Invalid or expired token, you may login and try again!");
+            return;
+        } catch (Exception e) {
+            response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+            response.getWriter().write(e.getMessage());
+            return;
+        }
+        filterChain.doFilter(request, response);
+    }
+
+    private String parseJwtToken(HttpServletRequest request) {
+        String header = request.getHeader("Authorization");
+        if(StringUtils.hasText(header) && header.startsWith("Bearer ")) {
+            return header.substring(7);
+        }
+        return null;
+    }
+}
---
+++ backend/src/main/java/com/bloodbank/backend/security/jwt/JwtUtils.java
@@ -0,0 +1,64 @@
+package com.bloodbank.backend.security.jwt;
+
+import com.bloodbank.backend.security.user.BBUserDetails;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import io.jsonwebtoken.security.SignatureException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.List;
+
+@Component
+public class JwtUtils {
+    @Value("${auth.token.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${auth.token.expirationInMils}")
+    private int expirationTime;
+
+    public String generateJwtToken(Authentication authentication) {
+        BBUserDetails userDetails = (BBUserDetails) authentication.getPrincipal();
+
+        List<String> roles = userDetails.getAuthorities().stream()
+                .map(GrantedAuthority::getAuthority).toList();
+
+        return Jwts.builder()
+                .setSubject(userDetails.getUsername())
+                .claim("id", userDetails.getId())
+                .claim("roles", roles)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + expirationTime))
+                .signWith(key(), SignatureAlgorithm.HS256).compact();
+    }
+
+    private Key key() {
+        return Keys.hmacShaKeyFor(Decoders.BASE64.decode(jwtSecret));
+    }
+
+    public String getUsernameFromJwtToken(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(key())
+                .build()
+                .parseClaimsJwt(token)
+                .getBody().getSubject();
+    }
+
+    public boolean validateJwtToken(String authToken) {
+        try {
+            Jwts.parserBuilder()
+                    .setSigningKey(key())
+                    .build()
+                    .parseClaimsJwt(authToken);
+            return true;
+        } catch (ExpiredJwtException | UnsupportedJwtException | MalformedJwtException | SignatureException |
+                 IllegalArgumentException e) {
+            throw new JwtException(e.getMessage());
+        }
+    }
+}
